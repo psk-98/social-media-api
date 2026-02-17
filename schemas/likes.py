50f54e6236975b33b056e1ef05e1f6dfb5e66c95
Paul <paul@vetro.co.za>
2026-02-17 19:19:01 +0200

    just adding a script for setting up linux will delete later

--- schemas/likes.py
+++ schemas/likes.py
@@ -5,3 +5,5 @@ class LikeRequest(BaseModel):
     user_id: str
     liked_id: str
     liked_type: str
+
+    model_config = {"from_attributes": True}
--- schemas/posts.py
+++ schemas/posts.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from schemas.likes import LikeRequest
 
