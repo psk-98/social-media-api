ba1280bda4cb921e376e1917ab86c6e9a237e73c
Paul <paul@vetro.co.za>
2026-03-24 21:13:21 +0200

    update docker compose to use .env for db setup

--- core/db.py
+++ core/db.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from core.settings import get_settings
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./social_media_db"
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://admin:password@social-network:5432/social_media_db"
-)
+# SQLALCHEMY_DATABASE_URL = (
+#     "postgresql://admin:password@social-network:5432/social_media_db"
+# )
 
 settings = get_settings()
 engine = create_engine(settings.DATABASE_URL)
