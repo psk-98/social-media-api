a1684e7b6670cc2f6e5f6208bc5474857e484b3e
Paul <paul@vetro.co.za>
2026-08-11 23:25:21 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,15 @@
 from functools import lru_cache
+from typing import Any
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",") if i.strip()]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 
 class Settings(BaseSettings):
